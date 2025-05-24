9221a52961d24f15c1670194cbd2b25acd133c53
Kiel Baluyut <kielbaluyut@Kiels-MacBook-Air.local>
2025-05-23 23:58:39 -0600

    Implemented Event List Page and Sortable Table Components

--- eventsui/src/app/app-routing.module.ts
+++ eventsui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventListComponent } from './features/events/event-list/event-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'events', component: EventListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- eventsui/src/app/app.module.ts
+++ eventsui/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
+import { EventListComponent } from './features/events/event-list/event-list.component';
+import { SortableTableComponent } from './shared/components/sortable-table/sortable-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    EventListComponent,
+    SortableTableComponent
   ],
   imports: [
     BrowserModule,
