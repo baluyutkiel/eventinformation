bbe2e8c00a1b39040bfba58f95e38c9ca1fe6947
Kiel Baluyut <kielbaluyut@Kiels-MacBook-Air.local>
2025-05-24 17:28:03 -0600

    Implementing sales sumary screen

--- eventsui/src/app/app-routing.module.ts
+++ eventsui/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventListComponent } from './features/events/event-list/event-list.component';
+import { SalesSummaryComponent } from './features/sales-summary/sales-summary.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/events', pathMatch: 'full' },
-  { path: 'events', component: EventListComponent }
+  { path: 'events', component: EventListComponent },
+  { path: 'sales-summary', component: SalesSummaryComponent }
 ];
 
 @NgModule({
--- eventsui/src/app/models/events.model.ts
+++ eventsui/src/app/models/events.model.ts
@@ -1,4 +1,4 @@
-export interface Event {
+export interface TickEvent {
     id: string;
     name: string;
     startDate: Date;
